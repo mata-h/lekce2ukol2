2769b446fcd38fde51ff1c9be00ccc6cba0175d0
Martina Heczko <heczkomartina@gmail.com>
2024-02-06 21:00:33 +0100

    Pridany script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+/* Delka filmu
+Jeden lístek do divadla :i[Pěst na oko] stojí 12 euro. 
+Spočítejte měsíční příjem divadla ze vstupného, přichází-li průměrně 174 návštěvníků na jedno představení 
+a divadlo hraje 15 představení měsíčně. Uložte výsledek do proměnné prijem.
+Hodnotu proměnné prijem vypište do stránky.
+
+Divadlo se rozhodlo prodávat studentské vstupné ve výši 65 % plného vstupného. 
+Jak se změní měsíční příjem divadla pokud víme, že 40 % návštěvníků jsou studenti?
+Vypište do stránky i příjem divadla se započítanou slevou pro studenty. */
+
